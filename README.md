2801abef09c70028dd5eece46ffb94b4ddf094dc
DogaTestere <testeredogayasemen2004@gmail.com>
2026-02-27 15:21:25 +0000

    Changed calculator file and added webpage code

---
+++ README.md
@@ -0,0 +1,6 @@
+**Açıklama:** `withWebPage` branch'inde web sayfası ekli ve python kodunda fonksiyonlar, açıklamaları ile birlikte bulunmakta.
+
+**Proje Amacı:** Python ile hesap makinesi oluşturma ve Basit HTML ve CSS sitesi
+
+**Kullanılan Teknolojiler**: VSCode kodu yazmak için, css için Qwen Code
+
--- proje/hesap_makinesi.py
+++ proje/hesap_makinesi.py
@@ -1,20 +1,43 @@
 print("--- Hesap Makinesi Uygulamasına Hoşgeldiniz ---")
+
 sayi1 = float(input("Lütfen 1. saynızı giriniz: "))
 sayi2 = float(input("Lütfen 2. sayınızı giriniz: "))
+# Normalde input sonucu çıkan değerler 'str' olarak yani 'yazı' olarak kaydedilir
+# 'str' tipi değişkenler toplanamaz, bu nedenle float() ile türünün sayıya dönüştürülmesi gerekir.
+# Bunu ya input() kısmını float() içine alarak yada değişkeni float() içine alarak yaparız
+
 islem = str(input("Lüften işleminizi giriniz( +, -, x, ÷) "))
+# Burada sadece ek önlem olarak tekrardan str yaptım.
+# Değişkenin türünü sabitlemek için yani, yoksa normalde de 'str' olarak belirleniyor tür
+
+def addition(num1, num2):
+    return num1 + num2
+# Burada işlem kısa olduğu ve sadece tek bir sonuç ortaya çıkacağı için direkt return kullanılabiliyor
+# Eğer fonksiyon içinde başka işlemler yapılacak olsaydı:
+# işlem_sonucu = ...
+# return işlem_sonucu
+# Yapılabilirdi
+
+def substraction(num1, num2):
+    return num1 - num2
+
+def multipilication(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    return num1 / num2
+# Eğer burada iki çizgi // kullanılsaydı, bölme işleminde sadece virgülden öncesi gelirdi
+# Tek çizgi kullanıldığında ise virgülden sonrasını da gösteriyor
+# Örn 1580 // 1440 --> 1.0
+#     1580 / 1440 --> 1.0972
 
 if islem == "+":
-    added_num = sayi1 + sayi2
-    print(f"Sonuç: {added_num}")
+    print(f"Sonuç: {addition(sayi1, sayi2)})
 elif islem == "-":
-    removed_num = sayi1 - sayi2
-    print(f"Sonuç: {removed_num}")
+    print(f"Sonuç: {substraction(sayi1, sayi2)}")
 elif islem == "x":
-    mult_num = sayi1 * sayi2
-    print(f"Sonuç: {mult_num}")
+    print(f"Sonuç {multipilication(sayi1, sayi2)}")
 elif islem == "÷":
-    div_num = sayi1 / sayi2
-    print(f"Sonuç: {div_num}")
+    print(f"Sonuç: {division(sayi1, sayi2)}")
 else:
     print("Geçersiz işlem girildi")
-
